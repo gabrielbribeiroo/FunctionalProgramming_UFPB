8a5e74fafb2f246cf0f8ee2838a168d675c8f80a
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-17 19:30:50 -0300

    understanding the list

---
+++ ch5-exemplos.hs
@@ -0,0 +1,40 @@
+-- Exemplos do Capítulo 5
+
+concatena :: [[a]] -> [a]
+concatena xss = [x | xs <- xss, x <- xs]
+
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+
+prime :: Int -> Bool
+prime n = factors n == [1,n]
+
+
+primes :: Int -> [Int]
+primes n = [x | x <- [2..n], prime x]
+
+
+pairs :: [a] -> [(a,a)]
+pairs xs = zip xs (tail xs)
+
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x,y) <- pairs xs]
+
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs =
+   [i | (x',i) <- zip xs [0..], x == x']
+
+
+count :: Char -> String -> Int
+count x xs = length [x' | x' <- xs, x == x']
+
+
+--permutações
+
+permut :: Eq a => [a] -> [[a]]
+permut [] = [[]]
+permut (x:xs) = [a:p | a <- (x:xs), p <- permut xs]

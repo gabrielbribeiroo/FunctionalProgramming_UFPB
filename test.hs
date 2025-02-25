06138af1ac62b3f9a1ad7247f1e1eb4e684b4b6a
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-02-25 20:05:29 -0300

    remove the last element

--- test.hs
+++ test.hs
@@ -8,9 +8,16 @@ factorial n = product [1..n]
 
 average ns = sum ns `div` length ns
 
+-- remove the last element
+my_last :: [a] -> a
+my_last xs = head (reverse x)
+
+my_init1 :: [a] -> a
+my_init1 xs = take (length xs - 1) xs
+
+my_init2 :: [a] -> a
+my_init2 xs = reverse (tail (reverse xs))
 
 -- extra 
-
 insert z f [] = z
 insert z f (x:xs) = f x (insert z f xs)
-

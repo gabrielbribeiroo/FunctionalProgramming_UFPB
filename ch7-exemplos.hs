ce3f35a521767abaf1816d42e23a4c7772746c52
Gabriel Barbosa Ribeiro de Oliveira <gabrielbroliveira@gmail.com>
2025-04-07 21:34:33 -0300

    Add exercises for all, any, takeWhile, dropWhile, foldr, map, filter, and dec2int functions

--- ch7-exemplos.hs
+++ ch7-exemplos.hs
@@ -248,15 +248,33 @@ winner' bs = case rank (rmempty bs) of
                (c:_) -> winner' (elim c bs)
 
 
+-- exercises
+all :: (a -> Bool) -> [a] -> Bool 
+all _ [] = True 
+all p (x:xs) = p x && all p xs
 
+any :: (a -> Bool) -> [a] -> Bool 
+any _ [] = False 
+any p (x:xs) = p x || any p xs 
 
+takeWhile :: (a -> Bool) -> [a] -> [a] 
+takeWhile _ [] = [] 
+takeWhile p (x:xs) 
+   | p x = x : takeWhile p xs 
+   | otherwise = [] 
 
+dropWhile :: (a -> Bool) -> [a] -> [a] 
+dropWhile _ [] = [] 
+dropWhile p (x:xs) 
+   | p x = dropWhile p xs 
+   | otherwise = x:xs 
 
+foldr :: (a -> b -> b) -> b -> [a] -> b
+map :: (a -> b) -> [a] -> [b] 
+map f = foldr (\x acc -> f x : acc) [] 
 
+filter :: (a -> Bool) -> [a] -> [a] 
+filter p = foldr (\x acc -> if p x then x : acc else acc) [] 
 
-
-
-
-
-
-
+dec2int :: [Int] -> Int 
+dec2int = foldl (\acc x -> acc * 10 + x) 0
